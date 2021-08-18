6f373c8f6e0db1a3fb62dc264205d5fbf76bb16b
mosa rabea <84705560+mosa-rabea@users.noreply.github.com>
2021-08-18 18:09:11 +0300

    Update weather.controller.js

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -8,7 +8,7 @@ let cache = new Cache();
 function weatherHandel(req, res) {
   let searchQue = req.query.cityName;
   let weatherData = {};
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQue}&key=${process.env.WEATHERIBT_API_KEY}&days=5`;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQue}&key=${process.env.WEATHERIBT_API_KEY}&days=5&language=en`;
   let key = `weather-${req.query.cityName}`;
 
   if (cache[key]) {
