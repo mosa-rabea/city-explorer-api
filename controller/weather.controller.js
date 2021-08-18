0588d6ecb23ec5b8de770cc299115f4fb1bd8ad9
mosa-rabea <mosa@protonmail.com>
2021-08-18 13:13:18 +0300

    lab10

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,17 +1,31 @@
 'use strict';
 const axios = require('axios');
 const Weather = require('../module/weather');
+const Cache = require('../module/Cache');
+
+let cache = new Cache();
+
 function weatherHandel(req, res) {
   let searchQue = req.query.cityName;
   let weatherData = {};
   let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQue}&key=${process.env.WEATHERIBT_API_KEY}&days=5`;
+  let key = `weather-${req.query.cityName}`;
 
-  axios.get(url).then((weatherRes) => {
-    weatherData = weatherRes.data.data.map((day) => {
-      return new Weather(day);
+  if (cache[key]) {
+    res.send(cache[key].data);
+  } else {
+    axios.get(url).then((weatherRes) => {
+      weatherData = weatherRes.data.data.map((day) => {
+        return new Weather(day);
+      });
+      cache[key] = {};
+      cache[key].timeStamp = Date.now();
+      cache[key].data = weatherData;
+
+      console.log(weatherData);
+      res.send(weatherData);
     });
-    console.log(weatherData);
-    res.send(weatherData);
-  });
+  }
+
 }
 module.exports = weatherHandel;
---
+++ module/Cache.js
@@ -0,0 +1,5 @@
+'use strict';
+
+class Cache{}
+
+module.exports=Cache;
